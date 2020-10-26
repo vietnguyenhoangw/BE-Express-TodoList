4b798cd1d6f5d6300e850f3f913c3ed2f9a6ba0f
vietnguyenhoangw <hoang-viet.nguyen@ant-tech.eu>
2020-10-26 18:23:37 +0700

    add remove task :penguin:

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
+const users = require("./users");
 
 const getTask = (_req, res, next) => {
   mongoose.model("Tasks").find({}, (err, task) => {
     if (err) {
-      return res.send(`errors: ${error}`
-      );
+      return res.send(`errors: ${error}`);
     }
-    res.send(task)
+    res.send(task);
   });
 };
 
@@ -24,9 +24,28 @@ const addTask = (req, res) => {
           `There was a problem adding the information to the database: ${error}`
         );
       }
-      res.send(task)
+      res.send(task);
     }
   );
 };
 
-module.exports = { getTask, addTask };
+const deleteTask = (req, res) => {
+  mongoose.model("Tasks").findById(req.params.id, (error, task) => {
+    console.log("deleteTask -> task", task);
+    if (error) {
+      return res.send(
+        `There was a problem reomve the information to the database: ${error}`
+      );
+    }
+    task.remove((err, task) => {
+      if (err) {
+        return res.send(
+          `There was a problem remove the information to the database: ${error}`
+        );
+      }
+      res.send('remove success !');
+    });
+  });
+};
+
+module.exports = { getTask, addTask, deleteTask };
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,6 +2,12 @@ const express = require("express"),
   router = express.Router(),
   taskController = require("../controllers/tasks");
 
-router.route("/").get(taskController.getTask).post(taskController.addTask);
+router.route("/")
+  .get(taskController.getTask)
+  .post(taskController.addTask);
+
+
+router.route('/:id')
+  .get(taskController.deleteTask);
 
 module.exports = router;
