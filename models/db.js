a9abdb20003041c87519aed89e5cebfe4ec7ee0c
vietnguyenhoangw <viet.nguyen@itbeesolutions.vn>
2021-01-11 00:46:33 +0700

    Config ip

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set('useCreateIndex', true); // This pervent collection.ensureIndex depreciation warning
 mongoose.set('useFindAndModify', false); // This prevent current mongoose depreciation warning
-mongoose.connect('mongodb://localhost/personal-blog', {
+mongoose.connect('mongodb://89.187.170.169/32/personal-blog', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
