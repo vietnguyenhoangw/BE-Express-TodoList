7231a499eacb1fc5849a25ab6f2710c45e8e1ab4
vietnguyenhoangw <viet.nguyen@itbeesolutions.vn>
2021-01-11 01:03:40 +0700

    deploy fix connect db

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set('useCreateIndex', true); // This pervent collection.ensureIndex depreciation warning
 mongoose.set('useFindAndModify', false); // This prevent current mongoose depreciation warning
-mongoose.connect('mongodb://localhost:27017/personal-blog', {
+mongoose.connect('mongodb+srv://personal-blog:<personal-blog>@cluster0.saevs.mongodb.net/<personal-blog>?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
