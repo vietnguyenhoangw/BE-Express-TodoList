9e7fd8357f2cc954d948b92881e4144c2e0bacd8
vietnguyenhoangw <viet.nguyen@itbeesolutions.vn>
2021-01-11 00:55:03 +0700

    config connect atlas

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set('useCreateIndex', true); // This pervent collection.ensureIndex depreciation warning
 mongoose.set('useFindAndModify', false); // This prevent current mongoose depreciation warning
-mongoose.connect('mongodb://localhost/personal-blog', {
+mongoose.connect('mongodb://localhost:27017/personal-blog', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
